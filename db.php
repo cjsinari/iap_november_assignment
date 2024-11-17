ae59eaeea9e746ead00109693d3334d5e1ee6e64
cjsinari <cyrilsinari@gmail.com>
2024-11-18 01:18:56 +0300

    updated database code

--- db.php
+++ db.php
@@ -3,21 +3,31 @@
 //Database connection
 
 class Database{
-    private $host = "localhost";
-    private $dbname = "diji_store";
-    private $username = "root";
-    private $password = "";
-    protected $connection;
+  private static $instance = null;
+  private $pdo;
 
-  public function __construct(){
-    try{
-        $this->connection = new PDO("mysql:host=$this->host;
-        dbname=$this->dbname", $this->username, $this->password);
-        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e){
-        die("Connection failed: " . $e->getMessage());
-    }
-    //return $this->connection;
+  private function __construct() {
+      try {
+          $this->pdo = new PDO(
+              'mysql:host=localhost;dbname=diji_store;charset=utf8',
+              'root', 
+              ''      
+          );
+          $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch (PDOException $e) {
+          die("Database connection failed: " . $e->getMessage());
+      }
+  }
+
+  public static function getInstance() {
+      if (self::$instance === null) {
+          self::$instance = new Database();
+      }
+      return self::$instance;
+  }
+
+  public function prepare($query) {
+      return $this->pdo->prepare($query);
   }
 
 }
