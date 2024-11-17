d632626904d4c509796a773cd4069d5de8996679
cjsinari <cyrilsinari@gmail.com>
2024-11-17 23:03:11 +0300

    Updated autoload

--- signup.php
+++ signup.php
@@ -1,8 +1,9 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php';
 require_once 'classes/user.php';
 require_once 'includes/mailer.php';
-require_once __DIR__ . '/vendor/autoload.php';
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 $username = $_POST['username'];
